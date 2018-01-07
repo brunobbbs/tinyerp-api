9f7b8665bfe3c358a9ff13621ad36a6a6b4652e2
Bruno Barbosa <bsbruno1@gmail.com>
2018-01-06 23:35:27 -0200

    tinyerp: add `contact_test`

--- tinyerp/tinyerp_test.go
+++ tinyerp/tinyerp_test.go
@@ -1,14 +1,9 @@
 package tinyerp
 
 import (
-	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	httpmock "gopkg.in/jarcoal/httpmock.v1"
 )
 
 func TestMustEnv(t *testing.T) {
@@ -33,46 +28,4 @@ func TestMustEnv(t *testing.T) {
 		return
 	}
 	t.Errorf("mustEnv() failed to fatal if env %q is not present", key)
-}
-
-func TestGetContactError(t *testing.T) {
-	assert := assert.New(t)
-	tiny := TinyERP{
-		baseURL: "https://tiny-api/test",
-		token:   "45bc28e1-24e8-41de-92fa-6bcccaf0cd80",
-		format:  "json",
-	}
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-	fixture, err := ioutil.ReadFile("./fixtures/contact-error.json")
-	if err != nil {
-		log.Fatal("Fixture file not found")
-	}
-	httpmock.RegisterResponder("POST", "https://tiny-api/test/contato.obter.php",
-		httpmock.NewStringResponder(200, string(fixture)))
-	cr, err := tiny.GetContact("122334ERR")
-	assert.Nil(err)
-	assert.Equal("Erro", cr.Response.Status)
-	assert.Equal("2", cr.Response.ErrorCode)
-}
-
-func TestGetContact(t *testing.T) {
-	assert := assert.New(t)
-	tiny := TinyERP{
-		baseURL: "https://tiny-api/test",
-		token:   "45bc28e1-24e8-41de-92fa-6bcccaf0cd80",
-		format:  "json",
-	}
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-	fixture, err := ioutil.ReadFile("./fixtures/contact-ok.json")
-	if err != nil {
-		log.Fatal("Fixture file not found")
-	}
-	httpmock.RegisterResponder("POST", "https://tiny-api/test/contato.obter.php",
-		httpmock.NewStringResponder(200, string(fixture)))
-	cr, err := tiny.GetContact("122334456")
-	assert.Nil(err)
-	assert.Equal("OK", cr.Response.Status)
-	assert.Equal("Contato Teste 3", cr.Response.Contact.Name)
 }
